c2005a58c5c0902bf224b997230d817c420ac294
unknown <mikrajewski@697-334-Edoras.sf.k12.internal>
2015-09-09 11:39:26 -0700

    dice

--- Dice.pde
+++ Dice.pde
@@ -1,4 +1,5 @@
 Die one;
+Die two;
 void setup()
 {
 	size(500,500);
@@ -7,8 +8,12 @@ void setup()
 void draw()
 {
 	background(0);
-	one = new Die(100,100);
+	one = new Die(1,1);
+	two = new Die(102,1);
 	one.show();
+	one.roll();
+	two.show();
+	two.roll();
 }
 void mousePressed()
 {
@@ -19,22 +24,61 @@ class Die //models one single dice cube
 	int dots,myX,myY;
 	Die(int x, int y) //constructor
 	{
-		dots = 1;
+		dots = (int)(Math.random()*6)+1;
 		myX = x;
 		myY = y;
 	}
 	void roll()
 	{
-		//your code here
+		if(dots == 1)
+		{
+			fill(0);
+			ellipse(myX+50,myY+50,15,15);
+		}
+		else if(dots == 2)
+		{
+			fill(0);
+			ellipse(myX+25,myY+25,15,15);
+			ellipse(myX+75,myY+75,15,15);
+		}
+		else if(dots == 3)
+		{
+			fill(0);
+			ellipse(myX+50,myY+50,15,15);
+			ellipse(myX+25,myY+25,15,15);
+			ellipse(myX+75,myY+75,15,15);
+		}
+		else if(dots == 4)
+		{
+			fill(0);
+			ellipse(myX+25,myY+25,15,15);
+			ellipse(myX+75,myY+75,15,15);
+			ellipse(myX+25,myY+75,15,15);
+			ellipse(myX+75,myY+25,15,15);
+		}
+		else if(dots == 5)
+		{
+			fill(0);
+			ellipse(myX+25,myY+25,15,15);
+			ellipse(myX+75,myY+75,15,15);
+			ellipse(myX+25,myY+75,15,15);
+			ellipse(myX+75,myY+25,15,15);
+			ellipse(myX+50,myY+50,15,15);
+		}
+		else if(dots == 6)
+		{
+			fill(0);
+			ellipse(myX+25,myY+25,15,15);
+			ellipse(myX+75,myY+75,15,15);
+			ellipse(myX+25,myY+75,15,15);
+			ellipse(myX+75,myY+25,15,15);
+			ellipse(myX+25,myY+50,15,15);
+			ellipse(myX+75,myY+50,15,15);
+		}
 	}
 	void show()
 	{
 		fill(255);
 		rect(myX,myY,100,100);
-		if(dots == 1)
-		{
-			fill(0);
-			ellipse(myX+50,myY+50,10,10);
-		}
 	}
 }
--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 public class Dice extends PApplet {
 
 Die one;
+Die two;
 public void setup()
 {
 	size(500,500);
@@ -23,8 +24,12 @@ public void setup()
 public void draw()
 {
 	background(0);
-	one = new Die(100,100);
+	one = new Die(1,1);
+	two = new Die(102,1);
 	one.show();
+	one.roll();
+	two.show();
+	two.roll();
 }
 public void mousePressed()
 {
@@ -35,23 +40,62 @@ class Die //models one single dice cube
 	int dots,myX,myY;
 	Die(int x, int y) //constructor
 	{
-		dots = 1;
+		dots = (int)(Math.random()*6)+1;
 		myX = x;
 		myY = y;
 	}
 	public void roll()
 	{
-		//your code here
+		if(dots == 1)
+		{
+			fill(0);
+			ellipse(myX+50,myY+50,15,15);
+		}
+		else if(dots == 2)
+		{
+			fill(0);
+			ellipse(myX+25,myY+25,15,15);
+			ellipse(myX+75,myY+75,15,15);
+		}
+		else if(dots == 3)
+		{
+			fill(0);
+			ellipse(myX+50,myY+50,15,15);
+			ellipse(myX+25,myY+25,15,15);
+			ellipse(myX+75,myY+75,15,15);
+		}
+		else if(dots == 4)
+		{
+			fill(0);
+			ellipse(myX+25,myY+25,15,15);
+			ellipse(myX+75,myY+75,15,15);
+			ellipse(myX+25,myY+75,15,15);
+			ellipse(myX+75,myY+25,15,15);
+		}
+		else if(dots == 5)
+		{
+			fill(0);
+			ellipse(myX+25,myY+25,15,15);
+			ellipse(myX+75,myY+75,15,15);
+			ellipse(myX+25,myY+75,15,15);
+			ellipse(myX+75,myY+25,15,15);
+			ellipse(myX+50,myY+50,15,15);
+		}
+		else if(dots == 6)
+		{
+			fill(0);
+			ellipse(myX+25,myY+25,15,15);
+			ellipse(myX+75,myY+75,15,15);
+			ellipse(myX+25,myY+75,15,15);
+			ellipse(myX+75,myY+25,15,15);
+			ellipse(myX+25,myY+50,15,15);
+			ellipse(myX+75,myY+50,15,15);
+		}
 	}
 	public void show()
 	{
 		fill(255);
 		rect(myX,myY,100,100);
-		if(dots == 1)
-		{
-			fill(0);
-			ellipse(myX+50,myY+50,10,10);
-		}
 	}
 }
   static public void main(String[] passedArgs) {
